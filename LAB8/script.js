d4505dc826962d10f46807fd00e15a8efe938be6
Yunchan94 <juleefang94@gmail.com>
2022-06-01 21:46:15 +0700

    them lab8 xoa 1 file

---
+++ LAB8/script.js
@@ -0,0 +1,8 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClosemodal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
