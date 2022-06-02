e80290f3cc56776df1642ae41d1873fb712ca36e
Yunchan94 <juleefang94@gmail.com>
2022-06-02 21:34:48 +0700

    sua lab7 them lab9

--- LAB7/script.js
+++ LAB7/script.js
@@ -1,15 +1,55 @@
 'use strict';
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct Number!🎉';
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-// console(document.querySelector('.guess').value);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
+
+  //When is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔No number!';
+    // document.querySelector('.message').textContent = '⛔No number!';
+    displayMessage('⛔No number!');
+    //When player win
+  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = 'Corret Number!🎉';
+    displayMessage('Corret Number!🎉');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    // When number incorrect
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.number').textContent =
+      //   guess > secretNumber ? 'Too high!👇' : 'Too low!👆';
+
+      displayMessage(guess > secretNumber ? 'Too high!👇' : 'Too low!👆');
+
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = 'You lost the game!😝';
+
+      displayMessage('You lost the game!😝');
+      document.querySelector('.score').textContent = 0;
+    }
   }
+});
+
+//again button
+document.querySelector('.again').addEventListener('click', function () {
+  const secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = '20';
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
--- LAB9/script.js
+++ LAB9/script.js
@@ -1,1 +1,41 @@
 'use strict';
+//Select element
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Start
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+const score = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+// Rolling dice
+
+btnRoll.addEventListener('click', function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    //add dice to current score
+    currentScore = currentScore + dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    current0El.textContent = currentScore;
+  } else {
+    //switch player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
