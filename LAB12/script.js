2ba739abc98d286dda9a97cccbff3709f2ce0682
YunChan94 <juleefang94@gmail.com>
2022-07-09 16:43:22 +0700

    sua lai 12.2, 12.3

--- LAB12/script.js
+++ LAB12/script.js
@@ -18,20 +18,41 @@ function dem(years, index, jk) {
 jk.forEach(dem);
 
 //12.2: Chuyển đổi tuổi thú cưng
-const data = [16, 6, 10, 5, 6, 1, 4];
-function calcAverageHumanAge() {
-  data.forEach(function (dogAge, index, data) {
+const data1 = [16, 6, 10, 5, 6, 1, 4];
+const data2 = [5, 2, 4, 1, 15, 8, 3];
+function calcAverageHumanAge(data) {
+  const humanAgeArr = data.map(function (dogAge) {
     //Tính tuổi của chó theo năm
     let humanAge;
     dogAge <= 2 ? (humanAge = 2 * dogAge) : (humanAge = 16 + dogAge * 4);
-    //Loại trừ tất cả những chú chó có humanAge dưới 18
-    if (humanAge < 18) data.splice(index, 1);
+    return humanAge;
   });
-  console.log(data);
-  //Tính humanage trung bình của các chú chó trưởng thành
-  const sum = data.reduce((sum, dogAge) => sum + dogAge, 0);
-  const average = (sum / data.length).toFixed(2);
 
+  //Loại trừ tất cả những chú chó có humanAge dưới 18
+  const under18 = humanAgeArr.filter(age => age > 18);
+
+  //Tính humanage trung bình của các chú chó trưởng thành
+  const sum = under18.reduce((sum, dogAge) => sum + dogAge, 0);
+  const average = (sum / under18.length).toFixed(2);
   console.log(average);
 }
-calcAverageHumanAge();
+calcAverageHumanAge(data1);
+calcAverageHumanAge(data2);
+// 12.3 Sử dụng Arrow Function
+function calcAverageHumanAge2(data) {
+  const under18 = data
+    .map(function (dogAge) {
+      //Tính tuổi của chó theo năm
+      let humanAge;
+      dogAge <= 2 ? (humanAge = 2 * dogAge) : (humanAge = 16 + dogAge * 4);
+      return humanAge;
+    })
+    //Loại trừ tất cả những chú chó có humanAge dưới 18
+    .filter(age => age > 18);
+  //Tính humanage trung bình của các chú chó trưởng thành
+  const sum = under18.reduce((sum, dogAge) => sum + dogAge, 0);
+  const average = (sum / under18.length).toFixed(2);
+  console.log(average);
+}
+calcAverageHumanAge2(data1);
+calcAverageHumanAge2(data2);
